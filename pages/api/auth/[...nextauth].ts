0e467ab058beedd322e8ea08708f2b99b93e8631
nmandal <nicholas.mandal@gmail.com>
2022-01-19 11:27:24 -0600

    pls

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,122 +1,121 @@
-// import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-// import { verifyPassword, hashPassword } from "../../../lib/auth/passwords";
-// import { Session } from "../../../lib/auth/session";
-// import prisma from "../../../lib/prisma";
+import { verifyPassword, hashPassword } from "../../../lib/auth/passwords";
+import { Session } from "../../../lib/auth/session";
+import prisma from "../../../lib/prisma";
 
-// export default NextAuth({
-//   adapter: PrismaAdapter(prisma),
-//   secret: process.env.NEXTAUTH_SECRET,
-//   // session: {
-//   //   jwt: true,
-//   // },
-//   pages: {
-//     signIn: "/sign-in",
-//     // signOut: "/auth/logout",
-//     // error: "/auth/error", // Error code passed in query string as ?error=
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       id: "app-login",
-//       name: "App Login",
-//       credentials: {
-//         email: {
-//           label: "Email Address",
-//           type: "email",
-//           placeholder: "john.doe@example.com",
-//         },
-//         password: {
-//           label: "Password",
-//           type: "password",
-//           placeholder: "Your super secure password",
-//         },
-//       },
-//       async authorize(credentials) {
-//         try {
-//           let maybeUser = await prisma.user.findFirst({
-//             where: {
-//               email: credentials.email,
-//             },
-//             select: {
-//               id: true,
-//               email: true,
-//               password: true,
-//               name: true,
-//               role: true,
-//             },
-//           });
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  // session: {
+  //   jwt: true,
+  // },
+  pages: {
+    signIn: "/sign-in",
+    // signOut: "/auth/logout",
+    // error: "/auth/error", // Error code passed in query string as ?error=
+  },
+  providers: [
+    CredentialsProvider({
+      id: "app-login",
+      name: "App Login",
+      credentials: {
+        email: {
+          label: "Email Address",
+          type: "email",
+          placeholder: "john.doe@example.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Your super secure password",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          let maybeUser = await prisma.user.findFirst({
+            where: {
+              email: credentials.email,
+            },
+            select: {
+              id: true,
+              email: true,
+              password: true,
+              name: true,
+              role: true,
+            },
+          });
 
-//           if (!maybeUser) {
-//             if (!credentials.password || !credentials.email) {
-//               throw new Error("Invalid Credentials");
-//             }
+          if (!maybeUser) {
+            if (!credentials.password || !credentials.email) {
+              throw new Error("Invalid Credentials");
+            }
 
-//             maybeUser = await prisma.user.create({
-//               data: {
-//                 email: credentials.email,
-//                 password: await hashPassword(credentials.password),
-//               },
-//               select: {
-//                 id: true,
-//                 email: true,
-//                 password: true,
-//                 name: true,
-//                 role: true,
-//               },
-//             });
-//           } else {
-//             const isValid = await verifyPassword(
-//               credentials.password,
-//               maybeUser.password
-//             );
+            maybeUser = await prisma.user.create({
+              data: {
+                email: credentials.email,
+                password: await hashPassword(credentials.password),
+              },
+              select: {
+                id: true,
+                email: true,
+                password: true,
+                name: true,
+                role: true,
+              },
+            });
+          } else {
+            const isValid = await verifyPassword(
+              credentials.password,
+              maybeUser.password
+            );
 
-//             if (!isValid) {
-//               throw new Error("Invalid Credentials");
-//             }
-//           }
+            if (!isValid) {
+              throw new Error("Invalid Credentials");
+            }
+          }
 
-//           return {
-//             id: maybeUser.id,
-//             email: maybeUser.email,
-//             name: maybeUser.name,
-//             role: maybeUser.role,
-//           };
-//         } catch (error) {
-//           console.log(error);
-//           throw error;
-//         }
-//       },
-//     })
-//   ],
-//   callbacks: {
-//     async signIn({ user, account, profile, email, credentials }) {
-//       return true;
-//     },
-//     async redirect({ url, baseUrl }) {
-//       return url.startsWith(baseUrl) ? url : baseUrl;
-//     },
-//     async jwt({ token, user, account, profile, isNewUser }) {
-//       if (user) {
-//         token.id = user.id;
-//         token.role = user.role;
-//       }
+          return {
+            id: maybeUser.id,
+            email: maybeUser.email,
+            name: maybeUser.name,
+            role: maybeUser.role,
+          };
+        } catch (error) {
+          console.log(error);
+          throw error;
+        }
+      },
+    })
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
 
-//       return token;
-//     },
-//     async session({ session, token, user }) {
-//       const sess: Session = {
-//         ...session,
-//         user: {
-//           ...session.user,
-//           id: token.id as string,
-//           role: token.role as string,
-//         },
-//       };
+      return token;
+    },
+    async session({ session, token, user }) {
+      const sess: Session = {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id as string,
+          role: token.role as string,
+        },
+      };
 
-//       return sess;
-//     },
-//   },
-// });
-export {}
+      return sess;
+    },
+  },
+});
